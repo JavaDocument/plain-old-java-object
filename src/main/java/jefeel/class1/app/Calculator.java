3cb5e8ccb4952f98c7a5aa7ec1f9a9a0f0502d18
JeFeel <jaephil94@gmail.com>
2023-07-15 06:39:59 +0900

    [feature] Jefeel 계산기 구현 테스트

---
+++ src/main/java/jefeel/class1/app/Calculator.java
@@ -0,0 +1,13 @@
+package jefeel.class1.app;
+
+
+public class Calculator {
+
+    public int add(int[] numList){
+        int result = 0;
+        for (int element : numList){
+            result+=element;
+        }
+        return result;
+    }
+}
