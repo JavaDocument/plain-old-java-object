3ca6c36734688ffefa135096da878e72882e1ae0
emilywin825 <emilywin825@naver.com>
2023-07-15 19:29:43 +0900

    정수의 합 구하기 구현

--- src/main/java/emilywin/class1/app/Main.java
+++ src/main/java/emilywin/class1/app/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a=0,b=0,sum=0;
         System.out.print("원하시는 첫번째 정수를 입력해주세요 : ");
@@ -27,8 +27,5 @@ public class Main {
 //        b=sc.nextInt();
 //        sum=a+b;
 //        System.out.println("입력하신 두개의 정수의 합은 "+sum+"입니다.");
-        long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
-        long secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
-        System.out.println("시간차이(m) : "+secDiffTime);
     }
 }
