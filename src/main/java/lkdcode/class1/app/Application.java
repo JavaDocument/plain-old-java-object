59e8d289ade0897bc9fa524d90a19c5e0bcf5cb9
lkdcode <dlrlejr1@naver.com>
2023-07-15 16:04:30 +0900

    [feat] appconfig

--- src/main/java/lkdcode/class1/app/Application.java
+++ src/main/java/lkdcode/class1/app/Application.java
@@ -1,36 +1,15 @@
 package lkdcode.class1.app;
 
-import lkdcode.class1.app.domain.calculator.CalculationItems;
-import lkdcode.class1.app.domain.calculator.Calculator;
 import lkdcode.class1.app.domain.menu.Menu;
-import lkdcode.class1.app.domain.util.IntConverterImpl;
-import lkdcode.class1.app.domain.validate.ValidImpl;
-import lkdcode.class1.app.domain.view.Console;
-import lkdcode.class1.app.domain.view.io.ReaderImpl;
-import lkdcode.class1.app.domain.view.io.WriterImpl;
+import lkdcode.class1.app.global.config.AppConfig;
 
 
 public class Application {
     public static void main(String[] args) {
-        var reader = new ReaderImpl();
-        var writer = new WriterImpl();
-        var items = new CalculationItems();
-        var valid = new ValidImpl();
-        var inputCount = 2;
-
-        Console console = new Console.Builder()
-                .reader(reader)
-                .writer(writer)
-                .items(items)
-                .valid(valid)
-                .inputCount(inputCount)
-                .build();
-
-        var converter = new IntConverterImpl();
-        var calculator = new Calculator(converter);
+        var console = AppConfig.console();
+        var calculator = AppConfig.calculator();
 
         var menu = new Menu(console, calculator);
-
         menu.start();
     }
 
