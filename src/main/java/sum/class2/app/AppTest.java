4e286781c6843f2dfbbd838d2c0da21a366ea29b
330sum <330sum@naver.com>
2023-07-19 23:55:46 +0900

    중복된 닉네임의 이메일 출력

--- src/main/java/sum/class2/app/AppTest.java
+++ src/main/java/sum/class2/app/AppTest.java
@@ -9,56 +9,58 @@ public class AppTest {
     public static List<String> findDuplicateNicknames(List<String> nicknameList) {
         List<String> result = new ArrayList<>();
         Map<String, Integer> countMap = new HashMap<>();
+        Map<String, String> nicknameEmailMap = new HashMap<>();
 
-        for (String name : nicknameList) {
-            for (int i = 0; i < name.length() - 1; i++) {
-                String twoChars = name.substring(i, i + 2);
-                System.out.println(twoChars);
+        for (String nickname : nicknameList) {
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String twoChars = nickname.substring(i, i + 2);
                 countMap.put(twoChars, countMap.getOrDefault(twoChars, 0) + 1);
             }
         }
 
+        for (Member member : getMembers()) {
 
-        for (String name : nicknameList) {
+            String nickname = member.getNickName();
+            String email = member.getEmail();
+
             boolean hasDuplicateTwoChars = false;
-            for (int i = 0; i < name.length() - 1; i++) {
-                String twoChars = name.substring(i, i + 2);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String twoChars = nickname.substring(i, i + 2);
                 if (countMap.get(twoChars) >= 2) {
                     hasDuplicateTwoChars = true;
                     break;
                 }
             }
             if (hasDuplicateTwoChars) {
-                result.add(name);
+                result.add(nickname);
+                nicknameEmailMap.put(nickname, email);
             }
         }
 
+        for (String nickname : result) {
+            System.out.println(nicknameEmailMap.get(nickname));
+        }
         return result;
-
     }
 
-
-    public static void main(String[] args) {
-
+    public static Set<Member> getMembers() {
         Set<Member> members = new HashSet<>();
         members.add(new Member("jm@email.com", "제이엠"));
         members.add(new Member("jason@email.com", "제이슨"));
         members.add(new Member("woniee@email.com", "워니"));
         members.add(new Member("mj@email.com", "엠제이"));
         members.add(new Member("nowm@email.com", "이제엠"));
+        return members;
+    }
 
 
+    public static void main(String[] args) {
 
         List<String> nicknameList = new ArrayList<>();
-        for (Member member : members) {
+        for (Member member : getMembers()) {
             nicknameList.add(member.getNickName());
         }
-
-        System.out.println("nicknameList = " + nicknameList);
-
         List<String> nicknames = findDuplicateNicknames(nicknameList);
-        System.out.println(nicknames);
-
 
 
     }
