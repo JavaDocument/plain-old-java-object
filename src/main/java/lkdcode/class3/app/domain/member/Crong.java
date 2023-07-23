aa251769288f01334fa99daf1b12c4235f253378
lkdcode <dlrlejr1@naver.com>
2023-07-23 19:40:09 +0900

    [feat] 게임에 참가하는 플레이어

---
+++ src/main/java/lkdcode/class3/app/domain/member/Crong.java
@@ -0,0 +1,22 @@
+package lkdcode.class3.app.domain.member;
+
+import lkdcode.class3.app.domain.page.Book;
+import lkdcode.class3.app.domain.page.Open;
+
+public class Crong implements Player {
+    private final Book book;
+
+    public Crong() {
+        this.book = openBook();
+    }
+
+    @Override
+    public Book openBook() {
+        return Open.newInstance();
+    }
+
+    @Override
+    public int getPlayerNumber() {
+        return book.getMaxNumber();
+    }
+}
---
+++ src/main/java/lkdcode/class3/app/domain/member/Player.java
@@ -0,0 +1,11 @@
+package lkdcode.class3.app.domain.member;
+
+import lkdcode.class3.app.domain.page.Book;
+
+public interface Player {
+
+    Book openBook();
+
+    int getPlayerNumber();
+
+}
