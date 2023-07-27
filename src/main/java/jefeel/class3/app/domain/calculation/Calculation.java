0f667c73a26da25e767ba3efd0a0fecd9753bbce
JeFeel <jaephil94@gmail.com>
2023-07-27 15:48:44 +0900

    [feat] class3 1차 완성

---
+++ src/main/java/jefeel/class3/app/domain/calculation/Calculation.java
@@ -0,0 +1,51 @@
+package jefeel.class3.app.domain.calculation;
+
+
+public class Calculation {
+
+    private int firstNumber;
+    private int secondNumber;
+
+    public Calculation() {
+        this.firstNumber = myScore(firstNumber);
+        this.secondNumber = myScore(secondNumber);
+    }
+
+
+    public static int addEachNumber(int number) {
+        int sum = 0;
+        String convertNum = String.valueOf(number);
+
+        char[] digit = convertNum.toCharArray();
+        for (char c : digit) {
+            sum += c - '0';
+        }
+
+        return sum;
+    }
+
+    public static int multiplyEachNumber(int number) {
+        int result = 1;
+        String convertNum = String.valueOf(number);
+
+        char[] digit = convertNum.toCharArray();
+        for (char c : digit) {
+            result = result * (c - '0');
+        }
+
+        return result;
+    }
+
+    public int compareNumber(int first, int second) {
+        return Math.max(first, second);
+    }
+
+
+    public int myScore(int number) {
+        int addition = addEachNumber(number);
+        int multiplication = multiplyEachNumber(number);
+
+        return compareNumber(addition, multiplication);
+    }
+
+}
