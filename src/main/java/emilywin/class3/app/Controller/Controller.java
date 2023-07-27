5ce48b29929b98078e95e2f4b6b77942bdbe687c
emilywin825 <emilywin825@naver.com>
2023-07-27 17:59:14 +0900

    3주차 과제 구현 완료

--- src/main/java/emilywin/class3/app/Controller/Controller.java
+++ src/main/java/emilywin/class3/app/Controller/Controller.java
@@ -1,19 +1,32 @@
 package emilywin.class3.app.Controller;
 
-import emilywin.class3.app.Input.BufferedReaderInputProcessor;
-import emilywin.class3.app.Input.InputProcessor;
-import java.io.IOException;
+import emilywin.class3.app.Service.Service;
+import java.util.Arrays;
+import java.util.List;
 
 public class Controller{
+    Service service = new Service();
+
     //pobi / crong 입력 받기
-    public Controller() throws IOException {
-        start();
+    public Controller(){
+        List<Integer> pobi = Arrays.asList(97, 98);
+        List<Integer> crong = Arrays.asList(197, 198);
+
+        int result = start(pobi, crong);
+
+        System.out.println("result : " + result);
     }
 
-    public void start(){
-        BufferedReaderInputProcessor bufferedReaderInputProcessor = new BufferedReaderInputProcessor();
-        System.out.print("pobi : ");
-        
-        System.out.print("crong : ");
+    public int start(List<Integer> pobi,List<Integer> crong){
+        int pobiBiggerNumber = service.checkException(pobi);//예외사항 체크
+        if(pobiBiggerNumber==-1) return -1;
+
+        int crongBiggerNumber = service.checkException(crong);//예외사항 체크
+        if(crongBiggerNumber==-1) return -1;
+
+        int result = service.checkWinner(pobiBiggerNumber, crongBiggerNumber);
+
+        return result;
     }
+
 }
--- src/main/java/emilywin/class3/app/Service/Service.java
+++ src/main/java/emilywin/class3/app/Service/Service.java
@@ -1,12 +1,79 @@
 package emilywin.class3.app.Service;
 
-public class Service {
-    public void convertStringtoList(){
+import java.util.ArrayList;
+import java.util.List;
 
+public class Service {
+
+    public int checkException(List<Integer> player) {
+        int leftPage = player.get(0), rightPage = player.get(1);
+        //이어져있는 페이지인지 체크 / 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수인지
+        if((rightPage - leftPage)!=1|| leftPage%2!=1|| rightPage%2!=0)
+            return -1;
+
+        return checkBiggerNumber(player);
     }
 
-    public void checkExceptions(){
+    public List<Integer> checkDigit(int pageNumber){
+        List<Integer> eachDigit = new ArrayList<>();
+        int onesDigit=0, tensDigit=0, hundredsDigit=0;
+        //한자리 수인지, 두자리 수인지,세자리 수인지 확인
+        int digit = (int) (Math.log10(pageNumber) + 1); //Math 함수를 사용해서 int의 길이 구하는 방법
 
+        switch (digit) {
+            case 1: //한자리 숫자일때(1~9)
+                eachDigit.add(pageNumber);
+                break;
+            case 2: //두자리 숫자일때(10~99)
+                tensDigit = pageNumber / 10;
+                onesDigit = pageNumber % 10;
+
+                eachDigit.add(tensDigit);
+                eachDigit.add(onesDigit);
+                break;
+            case 3: //세자리 숫자일때(100~400)
+                hundredsDigit = pageNumber / 100;
+                tensDigit = (pageNumber /10) % 10;
+                onesDigit = pageNumber % 10;
+
+                eachDigit.add(hundredsDigit);
+                eachDigit.add(tensDigit);
+                eachDigit.add(onesDigit);
+                break;
+        }
+
+        return eachDigit;
+    }
+
+    public int checkBiggerNumber(List<Integer> player){
+        int biggerNumber=0;
+        for (Integer playerPage: player) {
+            List<Integer> eachDigit = checkDigit(playerPage); //각 자리수 받음
+            //각 자리수 다 더하거나 곱하는 함수
+            int i = addAndMultiply(eachDigit);
+            if(biggerNumber<=i) biggerNumber=i;
+        }
+        return biggerNumber;
+    }
+
+    private int addAndMultiply(List<Integer> eachDigit) {
+        //왼쪽 페이지에서 더 큰 수와 오른쪽 페이지에서 더 큰 수 중 어떤것이 더 큰지를 비교해야 하는데 그걸 어떻게...??
+        int add=0, multiply=1,biggerNumber=0;
+
+        for (Integer ed :eachDigit) {
+            add+=ed;
+            multiply*=ed;
+        }
+        biggerNumber = add > multiply ? add : multiply;
+
+        System.out.println("biggerNumber : "+biggerNumber);
+        return biggerNumber;
+    }
+
+    public int checkWinner(int pobiBiggerNumber,int crongBiggerNumber){
+        if(pobiBiggerNumber>crongBiggerNumber) return 1;
+        else if(pobiBiggerNumber<crongBiggerNumber) return 2;
+        return 0;
     }
 
 }
