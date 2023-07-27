67d3200089df667ffb7ea73e2f11419dc15a49f5
kimjieung97 <dickseung23@naver.com>
2023-07-27 17:12:17 +0900

    # 지승 3주차 과제 구현..

---
+++ src/main/java/kimjiseung97/class3/app/Application.java
@@ -0,0 +1,31 @@
+package kimjiseung97.class3.app;
+
+import kimjiseung97.class3.app.domain.Service.Service;
+import kimjiseung97.class3.app.domain.controller.Controller;
+import kimjiseung97.class3.app.domain.member.User;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+//       Controller controller = new Controller();
+//
+//        String y = controller.selectCase("Y");
+//
+//        System.out.println(y);
+
+        User user = new User("크롱");
+
+        User user1 = new User("포비");
+
+        int maxNumber = user.findMaxNumber();
+
+        int maxNumber1 = user1.findMaxNumber();
+
+        System.out.println(maxNumber);
+        System.out.println(maxNumber1);
+
+
+    }
+}
---
+++ src/main/java/kimjiseung97/class3/app/domain/Book/OpenBook.java
@@ -0,0 +1,93 @@
+package kimjiseung97.class3.app.domain.Book;
+
+import kimjiseung97.class3.app.domain.member.Member;
+
+import java.util.*;
+
+public class OpenBook implements Book {
+
+    private static final int MAX_BOOK_SIZE = 400;
+    private Queue<Integer> bookPageQueue = new LinkedList<>();
+
+    private int leftPage;
+    private int rightPage;
+
+    private static int leftnumberMultiple;
+
+    private static int rightnumberMultiple;
+
+
+    public OpenBook() {
+        this.getBookPage();
+    }
+
+    @Override
+    public Queue<Integer> getBookPage() {
+        leftPage = getLeftPage();
+        rightPage = getRightPage();
+        bookPageQueue.clear();
+        bookPageQueue.add(leftPage);
+        bookPageQueue.add(rightPage);
+//        System.out.println(bookPageQueue);
+        return bookPageQueue;
+    }
+
+    @Override
+    public int getLeftsumNumber() {
+        leftnumberMultiple = getMuiltipleOfDigits(leftPage);
+        return getSumOfDigits(leftPage);
+    }
+
+    @Override
+    public int getRightsumNumber() {
+        rightnumberMultiple = getMuiltipleOfDigits(rightPage);
+        return getSumOfDigits(rightPage);
+    }
+
+    @Override
+    public int getLeftPage() {
+        return generateRandomOddNumber();
+    }
+
+    @Override
+    public int getRightPage() {
+
+        return leftPage + 1;
+    }
+
+    public static int getLeftnumberMultiple() {
+        return leftnumberMultiple;
+    }
+
+    public static int getRightnumberMultiple() {
+        return rightnumberMultiple;
+    }
+
+    private int generateRandomOddNumber() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(MAX_BOOK_SIZE) + 1;
+        if (randomNumber % 2 == 0) {
+            randomNumber++;
+        }
+        return randomNumber;
+    }
+
+    private int getSumOfDigits(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    private int getMuiltipleOfDigits(int number) {
+        int product = 1;
+        while (number > 0) {
+            product *= number % 10;
+            number /= 10;
+        }
+        return product;
+    }
+}
+
