5ee483f878cb3789260b6a909ae82168ce19a2b2
lenovo <golddrone@naver.com>
2023-08-01 10:19:14 +0900

    😊Class4 객체지향 적용X, 제한사항 적용X --- 구현한 내용 --- 사용자와 함께 아는 친구의 수 = 10점 사용자의 타임 라인에 방문한 횟수 = 1점 친구 추천 규칙에 따라 점수가 가장 높은 순으로 정렬하여 최대 5명을 return 추천 점수가 0점인 경우 추천하지 않으며, 추천 점수가 같은 경우는 이름순으로 정렬

---
+++ src/main/java/golddrone/class4/v2/User.java
@@ -0,0 +1,37 @@
+package golddrone.class4.v2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+    private String name;
+    private Map<String, Integer> friends;
+    public User() {
+        this.name = name;
+        this.friends = new HashMap<>();
+    }
+
+    public User(String name) {
+        this.name = name;
+        this.friends = new HashMap<>();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Integer> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Map<String, Integer> friends) {
+        this.friends = friends;
+    }
+
+
+}
