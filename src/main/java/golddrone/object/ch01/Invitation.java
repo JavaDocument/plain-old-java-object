c9fccb9f5f864b1fcce6e2b4843a7c5bd70f6fab
golddrone <golddrone@naver.com>
2023-08-22 12:54:01 +0900

    😊object ch01 code example😎

---
+++ src/main/java/golddrone/object/ch01/Invitation.java
@@ -0,0 +1,7 @@
+package golddrone.object.ch01;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ src/main/java/golddrone/object/ch01/TicketSeller.java
@@ -0,0 +1,13 @@
+package golddrone.object.ch01;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
