24a7c82ed676dd6b49d8924e97b2e5113373f4ea
330sum <330sum@naver.com>
2024-02-04 23:04:54 +0900

    feat [GoF] : structual pattern - Flyweight

---
+++ sum330/src/main/java/com/gof/structual/flyweight/step01/Character.java
@@ -0,0 +1,19 @@
+package com.gof.structual.flyweight.step01;
+
+class Character {
+
+    private char value;
+
+    private String color;
+
+    private String fontFamily;
+
+    private int fontSize;
+
+    public Character(char value, String color, String fontFamily, int fontSize) {
+        this.value = value;
+        this.color = color;
+        this.fontFamily = fontFamily;
+        this.fontSize = fontSize;
+    }
+}
